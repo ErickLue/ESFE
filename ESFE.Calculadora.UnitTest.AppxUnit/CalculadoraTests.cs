a122444125d85221d8948347b2d1ea5f327f5bd7
Isaiaslue42637 <Isaiaslue42637@outlook.com>
2024-10-10 09:30:05 -0600

    Agregar archivos de proyecto.

---
+++ ESFE.Calculadora.UnitTest.AppxUnit/CalculadoraTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESFE.Calculadora.UnitTest.AppxUnit
+{
+    public class CalculadoraTests
+    {
+        [Fact]
+        public void Add_ShoulReturnCorrectSum() {
+            //Arrange 
+            Calculator calculator = new Calculator();
+            double num1 = 5.0;
+            double num2 = 3.0;
+
+            // Act
+            double result = calculator.Add(num1, num2);
+
+            //Assert
+            Assert.Equal(8, result);
+        }
+
+
+        [Fact]
+        public void SubTract_ShoulReturnCorrectDifference()
+        {
+            //Arrange 
+            Calculator calculator = new Calculator();
+            double num1 = 8.0;
+            double num2 = 3.0;
+
+            // Act
+            double result = calculator.Subtract(num1, num2);
+
+            //Assert
+            Assert.Equal(5.0, result);
+        }
+
+
+        [Fact]
+        public void Multiply_ShoulReturnCorrectProduct()
+        {
+            //Arrange 
+            Calculator calculator = new Calculator();
+            double num1 = 4.0;
+            double num2 = 2.0;
+
+            // Act
+            double result = calculator.Multiply(num1, num2);
+
+            //Assert
+            Assert.Equal(8, result);
+        }
+
+        [Fact]
+        public void Divide_ShoulReturnCorrectQuotient()
+        {
+            //Arrange 
+            Calculator calculator = new Calculator();
+            double num1 = 10.0;
+            double num2 = 2.0;
+
+            // Act
+            double result = calculator.Divide(num1, num2);
+
+            //Assert
+            Assert.Equal(5.0, result);
+        }
+
+        [Fact]
+        public void Divide_ByZero_ShoulThrowException()
+        {
+            //Arrange 
+            Calculator calculator = new Calculator();
+            double num1 = 5.0;
+            double num2 = 0.0;
+
+            // Act
+           Assert.Throws<ArgumentException>(()=> calculator.Divide(num1, num2));
+
+        }
+    }
+}
